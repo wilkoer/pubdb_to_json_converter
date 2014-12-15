b297930545ba8797bc4115c274d9a9a83a35edec
wilkoer <alexander@Alexanders-MacBook-Pro.local>
2014-12-15 19:49:46 +0100

    main.js modified

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
 	
 	// create a new pubDB json object
 	var converter = new pubDB.json();
-
+ 
 	// initialize -> get a jQuery object of html contents in callback function
-	var dbObject = converter.init(function(dbObject) {
+	converter.init(function(dbObject) {
 		// pass dbObject to buildJSON method -> get a json object back (<- created on client side)
 		converter.buildPublicationJSON(dbObject, function(pubData) {
 			publicationsJSON = pubData;
