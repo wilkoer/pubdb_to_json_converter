385a1cfb7a7ccffdf1337233aefe6a963abe9d06
wilkoer <alexander@Alexanders-MacBook-Pro.local>
2014-12-15 19:53:16 +0100

    modified main.js

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
 	
 	// create a new pubDB json object
 	var converter = new pubDB.json();
-
+ 
 	// initialize -> get a jQuery object of html contents in callback function
-	var dbObject = converter.init(function(dbObject) {
+	converter.init(function(dbObject) {
 		// pass dbObject to buildJSON method -> get a json object back (<- created on client side)
 		converter.buildPublicationJSON(dbObject, function(pubData) {
 			publicationsJSON = pubData;
@@ -18,11 +18,13 @@ $(document).ready(function() {
 				authorsJSON = authorData;
 				//console.log(JSON.stringify(authorsJSON));
 
-				$('#publications').val(JSON.stringify(publicationsJSON)).show();
-				$('#authors').val(JSON.stringify(authorsJSON)).show();
-				$('h2').show();
 
 				$('img').hide();
+				$('span').text(new Date() - start + "ms");
+
+				$('h2').show();
+				$('#publications').val(JSON.stringify(publicationsJSON)).show();
+				$('#authors').val(JSON.stringify(authorsJSON)).show();
 			});
 		});
 	});
