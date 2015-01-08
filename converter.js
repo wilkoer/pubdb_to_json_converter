e61b5d050a2084633e73e534f6abfafd43b489c4
Alexander Schenker <aschenker@gmx.de>
2015-01-08 21:39:39 +0100

    closed unclosed brackets
    bugfix

--- converter.js
+++ converter.js
@@ -76,3 +76,4 @@ app.use(router);
 
 var server = app.listen(server_port, server_ip_address, function () {
   	console.log("Listening on " + server_ip_address + ", server_port " + server_port);
+});
